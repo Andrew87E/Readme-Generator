cf83ac8a2e1dc2a4d10a3649fb9299426169bda9
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-19 16:00:29 -0500

    final polish...contributors section not working...open to pull requests!

--- index.js
+++ index.js
@@ -37,11 +37,6 @@ const questions = [
     name: "demoLink",
   },
   {
-    type: "input",
-    message: "What is your email address?",
-    name: "contributing",
-  },
-  {
     type: "list",
     message: "Select license for your project",
     choices: ["MIT", "BSD-3", "APACHE-2.0", "GPL-3.0", "none"],
@@ -73,7 +68,7 @@ const questions = [
     message: "Were there any contributors?",
     name: "contributors",
     choices: [
-      { name: "Yes", value: "Yes" },
+      // { name: "Yes", value: "Yes" },
       { name: "No", value: "No" },
     ],
   },
@@ -145,7 +140,9 @@ init = () => {
 const myContReadme = (data) => {
   return `
   # ${data.title}
-  ${generateBadge.renderLicenseBadge(data.license)} 
+  ${generateBadge.renderLicenseBadge(data.license)}
+  [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
+
   ## Description
   ${data.description}
   ## Table of contents
@@ -154,7 +151,6 @@ const myContReadme = (data) => {
   - [Usage](#usage)
   - [Contributing](#contributing)
   - [Demo](#Demo)
-  - [Contributing](#contributing)
   ${generateBadge.renderLicenseLink(data.license)}
   - [Questions](#questions)
   ## Installation
@@ -163,12 +159,15 @@ const myContReadme = (data) => {
   ${data.usage}
   ${data.contResponse}
   ## Demo
-  See it in action on ${data.demoHost}!
-  ${data.demoLink}
-  ## How to Contribute
-  [Contributor Covenant](https://www.contributor-covenant.org/)
+  See it in action on [${data.demoHost}](${data.demoLink})!
+  ## Contributing
+  <details> 
+  <summary>Contributor Covenant</summary>
+  ${contCovenant}
+  </details><br>
+
   ## Questions
-  Shoot me an [Email](${data.email})
+  Shoot me an [Email](${data.email})<br>
   Check out my [Github](https://www.github.com/${data.username})!
   ${generateBadge.renderLicenseSection(data.license)}
         `;
@@ -180,7 +179,9 @@ const myContReadme = (data) => {
 const myReadme = (data) => {
   return `
   # ${data.title}
-${generateBadge.renderLicenseBadge(data.license)} 
+${generateBadge.renderLicenseBadge(data.license)}
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
+
 ## Description
 ${data.description}
 ## Table of contents
@@ -196,13 +197,161 @@ ${data.install}
 ## Usage
 ${data.usage}
 ## Demo
-See it in action on ${data.demoHost}!
-${data.demoLink}
+See it in action on [${data.demoHost}](${data.demoLink})!
+## Contributing
+<details> 
+<summary>Contributor Covenant</summary>
+${contCovenant}
+</details><br>
+
 ## Questions
-Shoot me an [Email](${data.email})
+Shoot me an [Email](${data.email})<br>
 Check out my [Github](https://www.github.com/${data.username})!
 ${generateBadge.renderLicenseSection(data.license)}
       `;
 };
 
 init();
+
+
+
+
+
+
+
+
+
+const contCovenant = `
+# Contributor Covenant Code of Conduct
+
+## Our Pledge
+
+We as members, contributors, and leaders pledge to make participation in our
+community a harassment-free experience for everyone, regardless of age, body
+size, visible or invisible disability, ethnicity, sex characteristics, gender
+identity and expression, level of experience, education, socio-economic status,
+nationality, personal appearance, race, caste, color, religion, or sexual
+identity and orientation.
+
+We pledge to act and interact in ways that contribute to an open, welcoming,
+diverse, inclusive, and healthy community.
+
+## Our Standards
+
+Examples of behavior that contributes to a positive environment for our
+community include:
+
+* Demonstrating empathy and kindness toward other people
+* Being respectful of differing opinions, viewpoints, and experiences
+* Giving and gracefully accepting constructive feedback
+* Accepting responsibility and apologizing to those affected by our mistakes,
+  and learning from the experience
+* Focusing on what is best not just for us as individuals, but for the overall
+  community
+
+Examples of unacceptable behavior include:
+
+* The use of sexualized language or imagery, and sexual attention or advances of
+  any kind
+* Trolling, insulting or derogatory comments, and personal or political attacks
+* Public or private harassment
+* Publishing others' private information, such as a physical or email address,
+  without their explicit permission
+* Other conduct which could reasonably be considered inappropriate in a
+  professional setting
+
+## Enforcement Responsibilities
+
+Community leaders are responsible for clarifying and enforcing our standards of
+acceptable behavior and will take appropriate and fair corrective action in
+response to any behavior that they deem inappropriate, threatening, offensive,
+or harmful.
+
+Community leaders have the right and responsibility to remove, edit, or reject
+comments, commits, code, wiki edits, issues, and other contributions that are
+not aligned to this Code of Conduct, and will communicate reasons for moderation
+decisions when appropriate.
+
+## Scope
+
+This Code of Conduct applies within all community spaces, and also applies when
+an individual is officially representing the community in public spaces.
+Examples of representing our community include using an official e-mail address,
+posting via an official social media account, or acting as an appointed
+representative at an online or offline event.
+
+## Enforcement
+
+Instances of abusive, harassing, or otherwise unacceptable behavior may be
+reported to the community leaders responsible for enforcement at
+[INSERT CONTACT METHOD].
+All complaints will be reviewed and investigated promptly and fairly.
+
+All community leaders are obligated to respect the privacy and security of the
+reporter of any incident.
+
+## Enforcement Guidelines
+
+Community leaders will follow these Community Impact Guidelines in determining
+the consequences for any action they deem in violation of this Code of Conduct:
+
+### 1. Correction
+
+**Community Impact**: Use of inappropriate language or other behavior deemed
+unprofessional or unwelcome in the community.
+
+**Consequence**: A private, written warning from community leaders, providing
+clarity around the nature of the violation and an explanation of why the
+behavior was inappropriate. A public apology may be requested.
+
+### 2. Warning
+
+**Community Impact**: A violation through a single incident or series of
+actions.
+
+**Consequence**: A warning with consequences for continued behavior. No
+interaction with the people involved, including unsolicited interaction with
+those enforcing the Code of Conduct, for a specified period of time. This
+includes avoiding interactions in community spaces as well as external channels
+like social media. Violating these terms may lead to a temporary or permanent
+ban.
+
+### 3. Temporary Ban
+
+**Community Impact**: A serious violation of community standards, including
+sustained inappropriate behavior.
+
+**Consequence**: A temporary ban from any sort of interaction or public
+communication with the community for a specified period of time. No public or
+private interaction with the people involved, including unsolicited interaction
+with those enforcing the Code of Conduct, is allowed during this period.
+Violating these terms may lead to a permanent ban.
+
+### 4. Permanent Ban
+
+**Community Impact**: Demonstrating a pattern of violation of community
+standards, including sustained inappropriate behavior, harassment of an
+individual, or aggression toward or disparagement of classes of individuals.
+
+**Consequence**: A permanent ban from any sort of public interaction within the
+community.
+
+## Attribution
+
+This Code of Conduct is adapted from the [Contributor Covenant][homepage],
+version 2.1, available at
+[https://www.contributor-covenant.org/version/2/1/code_of_conduct.html][v2.1].
+
+Community Impact Guidelines were inspired by
+[Mozilla's code of conduct enforcement ladder][Mozilla CoC].
+
+For answers to common questions about this code of conduct, see the FAQ at
+[https://www.contributor-covenant.org/faq][FAQ]. Translations are available at
+[https://www.contributor-covenant.org/translations][translations].
+
+[homepage]: https://www.contributor-covenant.org
+[v2.1]: https://www.contributor-covenant.org/version/2/1/code_of_conduct.html
+[Mozilla CoC]: https://github.com/mozilla/diversity
+[FAQ]: https://www.contributor-covenant.org/faq
+[translations]: https://www.contributor-covenant.org/translations
+`;
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -26,11 +26,3 @@ module.exports = {
 };
 
 
-/* 
-![License: MIT](https://img.shields.io/static/v1?label=License&message=MIT&color=yellow&style=for-the-badge)<br>
-![npm](https://img.shields.io/npm/v/npm?style=plastic&logo=nodedotjs)
-![npm](https://img.shields.io/badge/npm%20package-inquirer%406.3.1-brightgreen?style=plastic&logo=nodedotjs)<br>
-![GitHub issues](https://img.shields.io/github/issues/andrew87e/Readme-Generator?style=plastic)
-[Kurt Bixby](https://github.com/kurtbixby)
-
-*/
